c7a571bf38c227d2a6820731154364e276069d9a
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-26 16:51:30 +0530

    REFACTORED:using switch case,given weekday in number,print it in words

--- weekDay.js
+++ weekDay.js
@@ -1,20 +1,29 @@
 let input=require("readline-sync")
 let dayInNumber=input.questionInt("Enter weekday in Number");
-if(dayInNumber==1){
-	dayInWords="SUNDAY";
-}else if(dayInNumber==2){
-	dayInWords="MONDAY";
-}else if(dayInNumber==3){
-	dayInWords="THUESDAY"
-}else if(dayInNumber==4){
-	dayInWords="WEDNESDAY";
-}else if(dayInNumber==5){
-	dayInWords="THURSDAY";
-}else if(dayInNumbers==6){
-	dayInWords="FRIDAY";
-}else if(dayInNumbers==7){
-	dayInWords="SATURDAY";
-}else{
+
+switch(dayInNumber){
+	case 1:	
+		dayInWords="SUNDAY";
+		break;
+	case 2:
+		dayInWords="MONDAY";
+		break;
+	case 3:	
+		dayInWords="THUESDAY"
+		break;
+	case 4:
+		dayInWords="WEDNESDAY";
+		break;
+	case 5:
+		dayInWords="THURSDAY";
+		break;
+	case 6:
+		dayInWords="FRIDAY";
+		break;
+	case 7:
+		dayInWords="SATURDAY";
+		break;
+	default:
 	console.log("Invalid day number");
 }
 
