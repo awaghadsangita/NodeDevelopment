0b8f58ccf98576b676c1158cfa00f16b0089197e
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-26 17:27:42 +0530

    ADDED:did some unit conversion

---
+++ UnitConversion.js
@@ -0,0 +1,11 @@
+let oneFeet=12;
+let oneInch=(1/12)
+let fortyTwoInch=42*oneInch
+console.log("42 Inch :"+fortyTwoInch+"Feet");
+
+let totalArea=60*40;
+let areaInMeters=totalArea/0.3
+console.log("60 feet *40 feet :"+areaInMeters+"meters");
+
+let twentyFivePlotsArea=25*areaInMeters*0.00024;
+console.log("25 plots of 60 feet*40 feet area :"+twentyFivePlotsArea+"acres");
