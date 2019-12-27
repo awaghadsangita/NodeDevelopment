a5c0015203f3140703bcd50b46324e2d8031e02f
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-27 19:11:24 +0530

    ADDED:Extend the Flip Coin problem till either Heads or Tails wins 11 times

--- flipCoin.js
+++ flipCoin.js
@@ -1,8 +1,17 @@
 let HEAD=1;
-let coin=Math.floor(Math.random()*10)%2;
-
-if(coin==HEAD){
-	console.log("HEAD");
-}else{
-	console.log("TAIL");
+let headCount=0;
+let tailCount=0;
+while(true){
+	let coin=Math.floor(Math.random()*10)%2;
+	if(coin==HEAD){
+		headCount++;
+	}else{
+		tailCount++;
+	}
+	if(headCount==11 || tailCount==11){
+		break;
+	}
 }
+
+console.log("HEAD count :"+headCount);
+console.log("TAIL count :"+tailCount);
