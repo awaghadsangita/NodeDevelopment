cde9a913c952c8ece7643676bb49686997107ab5
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-27 15:09:39 +0530

    ADDED:find die rolled maximum and minimum times

--- dieMapProblems.js
+++ dieMapProblems.js
@@ -1,10 +1,40 @@
 let die=Math.floor(Math.random()*10)%6+1;
-let repeatationNumber=Math.floor(Math.random()*10);
 let dieRollResultDictionary={};
+let max;
+let dieRolledMax;
+let dieRolledMin;
 
-for(let i=0;i<repeatationNumber;i++){
+for(let i=1;i<=6;i++)
+{
+	dieRollResultDictionary[i]=0;
+}
+
+for(let i=0;i!=null;i++){
 	die=Math.floor(Math.random()*10)%6+1;
-	dieRollResultDictionary[i]=die
+	dieRollResultDictionary[die]=dieRollResultDictionary[die]+1;
+	for(let j=1;j<=6;j++)
+	{
+		if(dieRollResultDictionary[j]>max)
+		{
+			max=dieRollResultDictionary[j];
+		}
+	}
+	if(max>10)
+	break;
+}
+
+maximum=dieRollResultDictionary[1];
+minimum=dieRollResultDictionary[1];
+
+for(let i=1;i<=6;i++){
+	if(maximum<dieRollResultDictionary[i]){
+		maximum=dieRollResultDictionary[i];
+		dieRolledMax=i;
+	}
+	if(minimum>dieRollResultDictionary[i]){
+		minimum=dieRollResultDictionary[i];
+		dieRolledMin=i;
+	}
 }
 
 
