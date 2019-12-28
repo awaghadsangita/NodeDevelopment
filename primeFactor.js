42c6bd1e93e10bede60a2fb32f01dbf8ad0e4ec4
sangita awaghad <sangita.awaghad@gmail.com>
2019-12-28 10:29:22 +0530

    REFACTORED:used array to store prime factor and print it

--- primeFactor.js
+++ primeFactor.js
@@ -1,15 +1,19 @@
 let number=parseInt(process.argv[2]);
-
+let primeFactorArray=[];
 for(i=1;number%2==0;number=number/2){
-	console.log(2+" ");
+	primeFactorArray.push(2)
 }
 
 for(let i=3;i*i<number;i++){
 	while(number%i==0){
-		console.log(i+" ");
+		primeFactorArray.push(i);
 		number=number/i;
 	}
 }
 if(number>2){
-	console.log(number+" ");
+	primeFactorArray.push(number);
+}
+
+for(let i=0;i<primeFactorArray.length;i++){
+	console.log(primeFactorArray[i]);
 }
